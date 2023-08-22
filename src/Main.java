363306f9b56ff037604a22ddbe27025246b9ce97
Aleinx <aleinx.official@gmail.com>
2023-08-22 07:47:17 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int nHoras, nDias, nHijos, AFP;
+        double tarifa;
+        double sBruto, boni, desc, sNeto;
+        String afiliado;
+
+        DecimalFormat f2 = new DecimalFormat("0.00");
+        //Entrada de Datos
+        nHoras = Integer.parseInt(JOptionPane.showInputDialog(null,"¿Cuantas horas diarias trabaja?","Entrada de Datos",JOptionPane.QUESTION_MESSAGE));
+        nDias = Integer.parseInt(JOptionPane.showInputDialog(null,"¿Cuantos dias trabaja?","Entrada de Datos",JOptionPane.QUESTION_MESSAGE));
+        nHijos = Integer.parseInt(JOptionPane.showInputDialog(null,"¿Cuantos hijos tiene?","Entrada de Datos",JOptionPane.QUESTION_MESSAGE));
+        tarifa = Double.parseDouble(JOptionPane.showInputDialog(null,"¿Cual es la tarifa?","Entrada de Datos",JOptionPane.QUESTION_MESSAGE));
+        //Selector yes or no
+        AFP = JOptionPane.showConfirmDialog(null,"¿Está afiliado a una AFP?","Entrada de Datos",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+
+        sBruto = nHoras*nDias*tarifa;
+        if (nHijos>5 && sBruto<1000){
+            boni = 0.25*sBruto;
+        }
+        else
+            boni=0.10*sBruto;
+        if (AFP==JOptionPane.YES_OPTION){
+            desc=0.12*(boni+sBruto);
+            afiliado = "SI";
+        }
+        else {
+            desc=0.15*(boni+sBruto);
+            afiliado = "NO";
+        }
+        sNeto=sBruto+boni-desc;
+        //Resultados
+        String mensaje = "Boleta de Pago\n";
+        mensaje+="El sueldo bruto es: " + f2.format(sBruto) + "\nLa Bonificación es: " +  f2.format(boni) + "\nEl descuento es: " + f2.format(desc) + "\nEl sueldo neto es: " + f2.format(sNeto) + "\n Afiliado a AFP: " + afiliado;
+        JOptionPane.showMessageDialog(null,mensaje,"Resultados",JOptionPane.INFORMATION_MESSAGE);
+    }
+}
